8aabec9ddcb7165e3b728f90a1239ca3aa4cab06
유승민 <@sm.yu>
2025-09-26 17:43:05 +0900

    no message

---
+++ GICore/src/main/java/org/gi/gICore/model/table/LogTables.java
@@ -0,0 +1,5 @@
+package org.gi.gICore.model.table;
+
+public class LogTables {
+    public static final String TRANSACTION_LOG = "transaction_log";
+}
---
+++ GICore/src/main/java/org/gi/gICore/model/table/UserTables.java
@@ -0,0 +1,10 @@
+package org.gi.gICore.model.table;
+
+public class UserTables {
+    private static String USERS_TABLE = "user_table";
+    private static String USER_WALLTES = "user_wallets";
+    private static String USER_MAILBOXS = "user_mailbox";
+
+    public static String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + USERS_TABLE + " (";
+
+}
