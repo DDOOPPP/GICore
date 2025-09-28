0a5a1ac6f5f23f3c03e82b747673a6236ce99783
DESKTOP-LHLGKOO\khona <khona08@gmail.com>
2025-09-28 19:39:47 +0900

    fh

--- GICore/src/main/java/org/gi/gICore/model/table/LogTables.java
+++ GICore/src/main/java/org/gi/gICore/model/table/LogTables.java
@@ -2,4 +2,12 @@ package org.gi.gICore.model.table;
 
 public class LogTables {
     public static final String TRANSACTION_LOG = "transaction_log";
+
+    public static final String CREATE_TRANSACTION_LOG ="CREATE TABLE IF NOT EXISTS " + TRANSACTION_LOG + " ("+
+            "player_id VARCHAR(36) NOT NULL," +
+            "type VARCHAR (30) NOT NULL DEFAULT 'NEW'" +
+            "amount DECIMAL (10,0) NOT NULL DEFAULT 0," +
+            "amount DECIMAL (10,0) NOT NULL DEFAULT 0," +
+            "amount DECIMAL (10,0) NOT NULL DEFAULT 0,";
+
 }
--- GICore/src/main/java/org/gi/gICore/model/table/UserTables.java
+++ GICore/src/main/java/org/gi/gICore/model/table/UserTables.java
@@ -1,11 +1,11 @@
 package org.gi.gICore.model.table;
 
 public class UserTables {
-    private static String USERS_TABLE = "user_table";
-    private static String USER_WALLTES = "user_wallets";
-    private static String USER_MAILBOXS = "user_mailbox";
+    public static final String USERS_TABLE = "user_table";
+    private static final String USER_WALLTES = "user_wallets";
+    private static final String USER_MAILBOXS = "user_mailbox";
 
-    public static String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + USERS_TABLE + " (" +
+    public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + USERS_TABLE + " (" +
             "player_id VARCHAR(36) UNIQUE NOT NULL," +
             "player_name VARCHAR(40) UNIQUE NOT NULL," +
             "guild_name VARCHAR(40) NOT NULL DEFAULT 'NONE'," +
@@ -19,4 +19,13 @@ public class UserTables {
             "create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," +
             "PRIMARY KEY (player_id))";
 
+
+    public static final String CREATE_WALLET  = "CREATE TABLE IF NOT EXISTS " + USER_WALLTES + " (" +
+            "player_id VARCHAR(36) NOT NULL," +
+            "balance DECIMAL(10,0) NOT NULL DEFAULT 0," +
+            "created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+            "updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
+            "FOREIGN KEY (player_id) REFERENCES "+ USERS_TABLE + "(player_id) ON DELETE CASCADE)";
+
+    public static final String CREATE_MAILBOX ="CREATE TABLE IF NOT EXISTS " + USER_MAILBOXS + " (" ;
 }
